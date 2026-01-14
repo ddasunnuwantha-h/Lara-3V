bc0c197092e66a53ff4d93f39e66007fa6d9a1a0
ddasunnuwantha-h <ddasunnuwantha@gmail.com>
2026-01-14 16:03:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
-PREFIX: process.env.PREFIX || ".",
+SESSION_ID: process.env.SESSION_ID || "yFRi0Bga#u1bMD3mD_al49RKeAlvm13d2LKqx_-jIyiGniM9GOqE", // Add your session id
+PREFIX: process.env.PREFIX || "#",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
-DEV: process.env.DEV || "94779062397",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94701698511",
+DEV: process.env.DEV || "94701698511",
 LANG: process.env.LANG || "EN", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
